033c5ddfb4c7bde03aa9eb920ab8a71db3ac5056
aaronlaursen@gmail.com <aaronlaursen@gmail.com@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2011-07-26 18:53:21 +0000

    query controller
    git-svn-id: https://macademia.googlecode.com/svn/branches/nbrviz@621 22e0cbe0-2fb8-11df-a735-6ff41f51f3aa

---
+++ Macademia/grails-app/controllers/org/macademia/nbrviz/QueryController.groovy
@@ -0,0 +1,31 @@
+package org.macademia.nbrviz
+
+import org.macademia.Graph
+import grails.converters.JSON
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: research
+ * Date: 7/26/11
+ * Time: 1:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class QueryController {
+
+  def json2Service
+  def similarity2Service
+
+  def json = {
+      def max
+      if(params.maxPerson){
+          max = params.maxPerson as int
+      }
+      else{
+          max = 25
+      }
+      Set<Long> qset = params.qset.tokanize("c_").collect({ it.toLong() })
+      Graph graph = similarity2Service.calculateQueryNeighbors(qset, max)
+      def data = json2Service.buildQueryCentricGraph(qset, graph)
+      render(data as JSON)
+  }
+}
