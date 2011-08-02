f776a5d48714891aba5e51a6508a1b33949bf9b6
BrandonAMaus <BrandonAMaus@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2011-08-02 16:48:35 +0000

    Made InterestClusters more object oriented.
    git-svn-id: https://macademia.googlecode.com/svn/branches/nbrviz@697 22e0cbe0-2fb8-11df-a735-6ff41f51f3aa

--- Macademia/web-app/js/nbrviz/interest.js
+++ Macademia/web-app/js/nbrviz/interest.js
@@ -13,15 +13,15 @@ macademia.nbrviz.interest.nodeRadius = 10;
  *  name : label for cluster or not?
  */
 function InterestCluster(params) {
-    this.relatedInterests = params.relatedInterests,
+    this.relatedInterests = params.relatedInterests;
     this.color = params.color || 0.1;
     this.paper = params.paper || macademia.nbrviz.paper;
 
     if(params.name) {
-        this.name = params.name,
+        this.name = params.name;
         this.hasCenter = true;
     } else {
-        this.name = this.retrieveClusterName(this.relatedInterests),
+        this.name = this.retrieveClusterName();
         this.hasCenter = false;
     }
 
@@ -79,27 +79,26 @@ InterestCluster.prototype.getBottomLayer = function() {
 
 
 /**
- * Takes an array of the names of related interests, finds the two shortest names and returns
- * a short string depending on the number of related interests. Ex: "interest1, interest2, ..."
- * @param relatedInterests an array of the names of related interests
+ * Finds the related interests with the two shortest names and returns a short string
+ * depending on the number of related interests. Ex: "interest1, interest2, ..."
  */
-InterestCluster.prototype.retrieveClusterName = function(relatedInterests) {
+InterestCluster.prototype.retrieveClusterName = function() {
     var clusterName = "";
     var shortOne = "";
     var shortTwo = "";
 
-    if(relatedInterests.length == 1) {
-        return relatedInterests[0].name;
-    } else if(relatedInterests == 2) {
-        shortOne = relatedInterests[0].name;
-        shortTwo = relatedInterests[1].name;
+    if(this.relatedInterests.length == 1) {
+        return this.relatedInterests[0].name;
+    } else if(this.relatedInterests == 2) {
+        shortOne = this.relatedInterests[0].name;
+        shortTwo = this.relatedInterests[1].name;
     } else {
-        for(var i in relatedInterests) {
-            if(relatedInterests[i].name.length < shortOne.length || shortOne == "") {
+        for(var i in this.relatedInterests) {
+            if(this.relatedInterests[i].name.length < shortOne.length || shortOne == "") {
                 shortTwo = shortOne;
-                shortOne = relatedInterests[i].name;
-            } else if(relatedInterests[i].name.length < shortTwo.length || shortTwo == "") {
-                shortTwo = relatedInterests[i].name;
+                shortOne = this.relatedInterests[i].name;
+            } else if(this.relatedInterests[i].name.length < shortTwo.length || shortTwo == "") {
+                shortTwo = this.relatedInterests[i].name;
             }
         }
     }
@@ -169,11 +168,11 @@ InterestCluster.prototype.listeners = function() {
     this.triggerSet.push(this.ring);
 
     if(this.hasCenter) {
-        this.dragInterest(this.relatedInterestNodes);
+        this.dragInterest();
     }
     var self = this;
     this.triggerSet.mouseover(function() {
-        self.cancelAnimations(self.relatedInterestNodes);
+        self.cancelAnimations();
         self.hiddenRing.animate({
             r: macademia.nbrviz.interest.clusterRadius + macademia.nbrviz.interest.nodeRadius * 2,
             x: self.xPos - macademia.nbrviz.interest.clusterRadius - macademia.nbrviz.interest.nodeRadius * 2,
@@ -181,14 +180,14 @@ InterestCluster.prototype.listeners = function() {
             width: macademia.nbrviz.interest.clusterRadius * 2 + macademia.nbrviz.interest.nodeRadius * 4,
             height: macademia.nbrviz.interest.clusterRadius * 2 + macademia.nbrviz.interest.nodeRadius * 4
         }, 0, "linear");
-        self.placeRelatedInterests(self.relatedInterestNodes);
+        self.placeRelatedInterests();
         self.ring.animate({r: macademia.nbrviz.interest.clusterRadius}, 800, "elastic");
         if(!self.hasCenter) {
             self.interest[3].hide();
         }
     });
     this.triggerSet.mouseout(function() {
-        self.cancelAnimations(self.relatedInterestNodes);
+        self.cancelAnimations();
         self.hiddenRing.animate({
             r: 0,
             x: self.xPos - macademia.nbrviz.interest.centerRadius,
@@ -196,7 +195,7 @@ InterestCluster.prototype.listeners = function() {
             width: macademia.nbrviz.interest.centerRadius * 2,
             height: macademia.nbrviz.interest.centerRadius * 2 + 20
         });
-        self.hideRelatedInterests(self.relatedInterestNodes);
+        self.hideRelatedInterests();
         self.ring.animate({r: 0}, 400, "backIn");
         if(!self.hasCenter) {
             self.interest[3].show();
@@ -204,13 +203,13 @@ InterestCluster.prototype.listeners = function() {
     });
 };
 
-InterestCluster.prototype.dragInterest = function(interestNodes) {
+InterestCluster.prototype.dragInterest = function() {
     var self = this;
     var start = function () {
         // storing original coordinates
-        $.each(interestNodes, function(i) {
-           for(var k = 0; k < interestNodes[i].length; k++) {
-                interestNodes[i][k].hide();     
+        $.each(self.relatedInterestNodes, function(i) {
+           for(var k = 0; k < self.relatedInterestNodes[i].length; k++) {
+                self.relatedInterestNodes[i][k].hide();
            }
         });
         self.ring.hide();
@@ -222,7 +221,7 @@ InterestCluster.prototype.dragInterest = function(interestNodes) {
     up = function () {
         self.xPos = self.interest.getX();
         self.yPos = self.interest.getY();
-        self.placeRelatedInterests(interestNodes);
+        self.placeRelatedInterests();
         self.ring.attr({cx: self.xPos, cy: self.yPos});
         self.ring.show();
     };
@@ -230,59 +229,53 @@ InterestCluster.prototype.dragInterest = function(interestNodes) {
     this.interest.drag(move, start, up);
 };
 
-InterestCluster.prototype.placeRelatedInterests = function(relatedInterestNodes) {
+InterestCluster.prototype.placeRelatedInterests = function() {
     var positions = macademia.nbrviz.calculateRelatedInterestPositions(this.relatedInterests, macademia.nbrviz.interest.clusterRadius, this.xPos, this.yPos);
     var nodePositions = positions[0];
     var textPositions = positions[1];
 
-    $.each(relatedInterestNodes, function(i){
-        for(var j = 0; j < relatedInterestNodes[i].length - 1; j++) {
-            relatedInterestNodes[i][j].show();
-            relatedInterestNodes[i][j].animate({
+    var self = this;
+//    alert("placeRelatedInterests");
+    $.each(this.relatedInterestNodes, function(i){
+        for(var j = 0; j < self.relatedInterestNodes[i].length - 1; j++) {
+            self.relatedInterestNodes[i][j].show();
+            self.relatedInterestNodes[i][j].animate({
                 cx: nodePositions[i][0],
                 cy: nodePositions[i][1],
                 x: nodePositions[i][0] - macademia.nbrviz.interest.nodeRadius,
                 y: nodePositions[i][1] - macademia.nbrviz.interest.nodeRadius
             }, 800, "elastic");
         }
-        relatedInterestNodes[i][relatedInterestNodes[i].length - 1].show();
-        relatedInterestNodes[i][relatedInterestNodes[i].length - 1].animate({x: textPositions[i][0], y: textPositions[i][1]}, 800, "elastic");
+        self.relatedInterestNodes[i][self.relatedInterestNodes[i].length - 1].show();
+        self.relatedInterestNodes[i][self.relatedInterestNodes[i].length - 1].animate({x: textPositions[i][0], y: textPositions[i][1]}, 800, "elastic");
     });
 };
 
-InterestCluster.prototype.cancelAnimations = function(relatedInterestNodes) {
+InterestCluster.prototype.cancelAnimations = function() {
     this.ring.stop();
     this.hiddenRing.stop();
-    $.each(relatedInterestNodes, function(i){
-        for(var j = 0; j < relatedInterestNodes[i].length - 1; j++) {
-            relatedInterestNodes[i][j].stop();
+    var self = this;
+    $.each(this.relatedInterestNodes, function(i){
+        for(var j = 0; j < self.relatedInterestNodes[i].length - 1; j++) {
+            self.relatedInterestNodes[i][j].stop();
         }
     });
 };
 
-InterestCluster.prototype.hideRelatedInterests = function(relatedInterestNodes) {
+InterestCluster.prototype.hideRelatedInterests = function() {
     var self = this;
-    $.each(relatedInterestNodes, function(i){
-        for(var j = 0; j < relatedInterestNodes[i].length - 1; j++) {
-            relatedInterestNodes[i][j].animate({
+    $.each(this.relatedInterestNodes, function(i){
+        for(var j = 0; j < self.relatedInterestNodes[i].length - 1; j++) {
+            self.relatedInterestNodes[i][j].animate({
                 cx: self.xPos,
                 cy: self.yPos,
                 x: self.xPos - macademia.nbrviz.interest.nodeRadius,
                 y: self.yPos - macademia.nbrviz.interest.nodeRadius
             }, 400, "backIn");
-            relatedInterestNodes[i][relatedInterestNodes[i].length  - 1].animate({x: self.xPos, y: self.yPos}, 400, "backIn", function(){
-                self.hideText(relatedInterestNodes)
+            self.relatedInterestNodes[i][self.relatedInterestNodes[i].length  - 1].animate({x: self.xPos, y: self.yPos}, 400, "backIn", function(){
+                self.hideText(self.relatedInterestNodes)
             });
         }
-    });
-};
-
-InterestCluster.prototype.resetNodePositions = function(relatedInterestNodes, nodePositions, textPositions) {
-    $.each(relatedInterestNodes, function(i) {
-        nodePositions[i][0] = relatedInterestNodes[i][1].attr("cx");
-        nodePositions[i][1] = relatedInterestNodes[i][1].attr("cy");
-        textPositions[i][0] = relatedInterestNodes[i][3].attr("x");
-        textPositions[i][1] = relatedInterestNodes[i][3].attr("y");
     });
 };
 
@@ -291,8 +284,8 @@ InterestCluster.prototype.resetNodePositions = function(relatedInterestNodes, no
  * @ param color of the relatedInterest (a hue between 0 and 1)
  * @ author Emi Lim
  */
-function Interest(params){
+function Interest(params) {
     this.id = params.id;
-    this.name = params.name,
+    this.name = params.name;
     this.color = params.color;
-};
+}
