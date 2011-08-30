227579960578eb09e8d822b5872207f79e090868
shilad@gmail.com <shilad@gmail.com@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2011-08-30 22:16:04 +0000

    Pulling in recent changes to trunk (including redesign).
    git-svn-id: https://macademia.googlecode.com/svn/branches/nbrviz@784 22e0cbe0-2fb8-11df-a735-6ff41f51f3aa

--- Macademia/web-app/js/nbrviz/query.js
+++ Macademia/web-app/js/nbrviz/query.js
@@ -19,6 +19,7 @@ macademia.nbrviz.initQueryViz = function(vizJson) {
         vizJson.interests[id].cluster = id;  // work around omission from json service...
     });
 
+    alert('here 0');
     $.each(vizJson.interests, function (id, info) {
         var hasCluster = (info.cluster && info.cluster >= 0);
         var color = -1;
@@ -32,6 +33,7 @@ macademia.nbrviz.initQueryViz = function(vizJson) {
         }
     });
 
+        alert('here 1');
     // Create interest clusters
     var queryInterests = {};
     $.each(vizJson.queries, function (i, id) {
@@ -45,6 +47,7 @@ macademia.nbrviz.initQueryViz = function(vizJson) {
         });
         queryInterests[id] = ic;
     });
+        alert('here 2');
 
     // Create people
     // TODO: incorporate interest similarity scores
@@ -116,6 +119,7 @@ macademia.nbrviz.initQueryViz = function(vizJson) {
         });
         people.push(person);
     });
+        alert('here 4');
 
     var qv = new QueryViz({
         people : people,
@@ -140,7 +144,6 @@ function QueryViz(params) {
     this.edges = [];
     this.highlighted = [];
 
-    // Set up the transparency filter
     this.fadeScreen = this.paper.rect(0, 0, this.paper.width, this.paper.height);
     this.fadeScreen.attr({ fill : 'white' , opacity : 0.0, 'stroke-width' : 0});
 
