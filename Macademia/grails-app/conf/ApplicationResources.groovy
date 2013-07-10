c42200bcdda5bda56e973c2b1394e730cb8a9153
Rebecca Gold <rgold1@macalester.edu>
2013-07-10 11:19:13 -0500

    Resolved conflict.

--- Macademia/grails-app/conf/ApplicationResources.groovy
+++ Macademia/grails-app/conf/ApplicationResources.groovy
@@ -48,7 +48,7 @@ modules = {
         resource url: '/js/d3test/label.js'
         resource url: '/js/d3test/interest.js'
         resource url: 'js/d3test/person-layout.js'
-        resource url: '/js/d3test/interest-layout.js'
+        resource url: '/js/d3test/interestLayout.js'
         resource url: '/js/d3test/person.js'
     }
 
