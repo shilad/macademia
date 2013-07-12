6a65bdf19a751b046965c415b9135f5623bbd00d
Margaret Giesel <arich@macalester.edu>
2013-07-12 14:15:02 -0500

    center node for new person centered layout

--- Macademia/grails-app/conf/ApplicationResources.groovy
+++ Macademia/grails-app/conf/ApplicationResources.groovy
@@ -51,6 +51,7 @@ modules = {
         resource url: 'js/d3test/person-layout.js'
         resource url: '/js/d3test/interest-layout.js'
         resource url: '/js/d3test/base-viz.js'
+        resource url: '/js/d3test/person-center.js'
     }
 
     d3demo {
