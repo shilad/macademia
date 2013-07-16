914fe2caf39bbac194c9efdb933da0e2a61dc584
jesse <jrussel1@macalester.edu>
2013-07-16 13:36:39 -0500

    interst viz

--- Macademia/web-app/js/d3test/interest-viz.js
+++ Macademia/web-app/js/d3test/interest-viz.js
@@ -5,3 +5,14 @@
  * Time: 1:25 PM
  * To change this template use File | Settings | File Templates.
  */
+
+var MC = (window.MC = (window.MC || {}));
+
+
+MC.interestViz = function(params) {
+    this.hubs = params.hubs;
+    this.people = params.people;
+    this.root = params.root;
+    this.svg = params.svg;
+
+};
