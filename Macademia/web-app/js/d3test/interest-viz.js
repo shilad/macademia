e087e1f35b773448f5ad185db38d1ac896f38c25
jesse <jrussel1@macalester.edu>
2013-07-16 14:02:05 -0500

    interst viz skeleton

--- Macademia/web-app/js/d3test/interest-viz.js
+++ Macademia/web-app/js/d3test/interest-viz.js
@@ -14,5 +14,44 @@ MC.interestViz = function(params) {
     this.people = params.people;
     this.root = params.root;
     this.svg = params.svg;
+};
 
+//Position the hubs and the root
+MC.interestViz.prototype.createInterestViz = function(){
+
+};
+
+
+
+/*
+ * Methods for the people heads are below here-----
+ */
+
+//return a person view
+MC.interestViz.prototype.createPersonView = function() {
+    this.personView = MC.person();
+    return this.personView;
+};
+
+//Creates the floating people heads
+MC.interestViz.prototype.createPeople = function() {
+    this.svg
+        .selectAll('g.person')
+        .data(this.people)
+        .enter()
+        .call(this.personView);
+};
+
+//Grabs all the people in svg
+MC.interestViz.prototype.getD3People = function() {
+    return this.svg.selectAll('g.person');
+}
+
+//Sets the locations of the person heads
+MC.interestViz.prototype.createPersonLayoutView = function(){
+    this.personLayoutView = MC.personLayout()
+        .setPeopleNodes(this.getD3People())
+        .setClusterMap(this.clusterMap)
+        .setInterestNodes(this.getD3Interests());
+    return this.personLayoutView;
 };
