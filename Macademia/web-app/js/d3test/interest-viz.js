7fa2556d0e98195fe6d306d453cb3a1291e3092e
Rebecca Gold <rgold1@macalester.edu>
2013-07-25 16:38:58 -0500

    commit to push

--- Macademia/web-app/js/d3test/interest-viz.js
+++ Macademia/web-app/js/d3test/interest-viz.js
@@ -408,14 +408,14 @@ MC.InterestViz.prototype.toolTipHover = function(){
 //    $("#interestToolTip")
 
 
-    var div = d3.select("div")
-        .style("opacity", 0);
+//    var div = d3.select("div")
+//        .style("opacity", 0);
 
-    console.log(div);
+//    console.log(div);
 
     this.svg.selectAll("g.interest")
         .on("mouseover", function(d){
-            d3.select("div").transition()
+            d3.select("div").style("opacity", 0).transition()
                 .duration(200)
                 .style("opacity", .9);
         })
@@ -423,6 +423,6 @@ MC.InterestViz.prototype.toolTipHover = function(){
             d3.select("div").transtion()
                 .duration(500)
                 .style("opacity", 0)
-        })
+        });
 };
 
