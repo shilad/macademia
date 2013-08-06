bc90507a242e29dafbdf83ed55ad6d32c28e1747
Sam Naden <arich@macalester.edu>
2013-08-06 14:30:25 -0500

    VizSurveyController set up to render basic views

--- Macademia/grails-app/controllers/org/macademia/VizSurveyController.groovy
+++ Macademia/grails-app/controllers/org/macademia/VizSurveyController.groovy
@@ -2,13 +2,24 @@ package org.macademia
 
 class VizSurveyController {
 
+    def consent() {
+        render(view: 'consent')
+    }
+
     def interest() {
+        render(view: 'interest')
+    }
+
+    def instructions() {
         render(view: 'instructions')
+    }
+
+    def recap() {
+        render(view: 'recapt')
     }
 
     def index() {
         render('Hello, world!')
     }
-
 
 }
