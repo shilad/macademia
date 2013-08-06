db02b8913280995b9a363377d46a4043a23a353e
Sam Naden <arich@macalester.edu>
2013-08-06 13:56:41 -0500

    pullin

---
+++ Macademia/grails-app/controllers/org/macademia/vizSurvey/ConsentController.groovy
@@ -0,0 +1,12 @@
+package org.macademia.vizSurvey
+
+class ConsentController {
+
+    def interest() {
+        render(view: 'instructions')
+    }
+
+    def index() {
+        render('Hello, world!')
+    }
+}
